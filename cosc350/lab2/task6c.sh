0c294fd035ded6c65c7c491b05b020abc21aa483
chiefdondjeu <florentdondjeu@yahoo.com>
2020-09-15 15:25:58 -0400

    remaining task 7,8,9

--- cosc350/lab2/task6c.sh
+++ cosc350/lab2/task6c.sh
@@ -9,5 +9,19 @@ while [ $numb -lt 5 ] || [ $numb -gt 9 ]; do
 	read numb
 done
 
-echo "done"
+x=$numb
+
+for i in $(seq 1 $numb); do
+	for ((k = x; k > 0; k--)); do
+		printf " "
+	done
+
+	for ((j = 1; j <= i; j++)); do
+		printf "$i ";
+	done
+
+	echo " "
+	let x--
+done
+
 exit 0
