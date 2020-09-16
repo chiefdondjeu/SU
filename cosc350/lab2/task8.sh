e6a53dc4b410b7c50a355eded3342a0968f62c73
chiefdondjeu <florentdondjeu@yahoo.com>
2020-09-15 21:12:10 -0400

    remaining task, 9

--- cosc350/lab2/task8.sh
+++ cosc350/lab2/task8.sh
@@ -1,3 +1,18 @@
-#!bin/sh
+#!/bin/sh
 #task8.sh
 
+if [ $# -lt 1 ]; then
+	echo "You need to pass one numerical argument"
+elif [ $# -gt 1 ]; then
+	echo "You only need one numerical argument"
+else
+	let size=${#1}-1
+	x=0
+	for i in $(seq 0 $size); do
+		y=${1:i:1}
+		let x=x+y
+	done
+	echo $x
+fi
+
+exit 0
